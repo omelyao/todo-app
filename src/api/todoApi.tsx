e24c6be8fa16c8fd29ee608e877015449e9b3cd1
Omelyao <ryt9992@mail.ru>
2026-04-26 12:04:58 +0500

    newBranch

---
+++ src/api/todoApi.tsx
@@ -0,0 +1,8 @@
+import { Todo } from '../features/todo/model/types';
+
+const URL = 'http://localhost:3001/todos';
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+export const todoApi = {};
